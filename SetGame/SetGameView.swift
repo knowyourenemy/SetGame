77809b6d761e36c2cbe99684db53749207d4f480
Aaditya Patwari <aadipatwari@gmail.com>
2023-12-09 15:35:51 +0800

    Basic CardView

--- SetGame/SetGameView.swift
+++ SetGame/SetGameView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  SetGameView.swift
 //  SetGame
 //
 //  Created by Aaditya Patwari on 9/12/23.
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct SetGameView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ForEach(0..<3) { _ in
+            CardView(
+                shape: Rectangle(),
+                count: 2,
+                color: .orange,
+                shade: .filled)
         }
-        .padding()
+        
+    }
+}
+
+
+enum Shade: Double {
+    case filled = 1
+    case partiallyFilled = 0.3
+    case empty = 0
+}
+
+struct CardView<ItemShape: Shape>: View {
+    let shape: ItemShape
+    let count: Int
+    let color: Color
+    let shade: Shade
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 12)
+            VStack() {
+                ForEach(0..<count, id: \.self) { _ in
+                    shape.aspectRatio(2, contentMode: .fit)
+                }
+            }.foregroundColor(color)
+                .opacity(shade.rawValue)
+                .padding(10)
+        }
+        .aspectRatio(2/3, contentMode: .fit)
+        .foregroundColor(.gray)
     }
 }
 
