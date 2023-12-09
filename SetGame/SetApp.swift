cde0ad7e730750b0ee70dc3242082f053d6a7217
Aaditya Patwari <aadipatwari@gmail.com>
2023-12-09 21:17:08 +0800

    Add basic ViewModel functionality.

--- SetGame/SetApp.swift
+++ SetGame/SetApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct SetApp: App {
+    @StateObject var viewModel = SetGameViewModel()
     var body: some Scene {
         WindowGroup {
-            SetGameView()
+            SetGameView(viewModel: viewModel)
         }
     }
 }
--- SetGame/SetGameViewModel.swift
+++ SetGame/SetGameViewModel.swift
@@ -5,6 +5,14 @@
 //  Created by Aaditya Patwari on 9/12/23.
 //
 
-import Foundation
+import SwiftUI
+
+class SetGameViewModel: ObservableObject {
+    @Published private var model = SetGameModel()
+    
+    var cards: Array<SetGameModel.Card> {
+        return model.cards
+    }
+}
 
 
