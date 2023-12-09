ce9767cfaa069c9426e794ad9da7b31083d72d99
Aaditya Patwari <aadipatwari@gmail.com>
2023-12-09 13:55:02 +0800

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Aaditya Patwari on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
